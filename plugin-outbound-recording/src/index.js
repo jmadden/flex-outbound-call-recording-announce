6bf0073496605aff7f94d93c40c1acf76ebaf1bb
Jim Madden <jmadden@twilio.com>
2021-12-07 13:01:28 -0800

    initial commit

---
+++ plugin-outbound-recording/src/index.js
@@ -0,0 +1,5 @@
+import * as FlexPlugin from 'flex-plugin';
+
+import OutboundRecordingPlugin from './OutboundRecordingPlugin';
+
+FlexPlugin.loadPlugin(OutboundRecordingPlugin);
---
+++ plugin-outbound-recording/src/utils/fetch.js
@@ -0,0 +1,31 @@
+import { Manager } from '@twilio/flex-ui';
+const manager = Manager.getInstance();
+
+const updateConference = async (conferenceSid, announceUrl) => {
+  console.debug('Updating Conference:', conferenceSid);
+  const fetchUrl = `${process.env.FLEX_APP_FUNCTIONS_BASE}/update-conference`;
+  const fetchBody = {
+    Token: manager.store.getState().flex.session.ssoTokenPayload.token,
+    conferenceSid,
+    announceUrl,
+  };
+  const fetchOptions = {
+    method: 'POST',
+    body: new URLSearchParams(fetchBody),
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+    },
+  };
+
+  let conference;
+  try {
+    const response = await fetch(fetchUrl, fetchOptions);
+    conference = await response.json();
+    console.debug('Updated Conference:', conference);
+  } catch (error) {
+    console.error('Failed to update conference');
+  }
+  return conference;
+};
+
+export default updateConference;
